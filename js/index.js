31257708ecf207ec11c0e30d9bd065ac5f87cf41
Angel <angeldavidquintanilla@gmail.com>
2022-08-15 21:36:34 -0600

    Teoría básica

---
+++ js/index.js
@@ -0,0 +1,56 @@
+//var es de alcance global
+//let tiene menos alcance
+//const cuando es una constante osea que su valor no cambia
+//prompt comple la funcion de un imputbox
+/*
+----------OPERADOES DE ASINGANCIÓN-----------------------------------------------
+x+=b es igual a tener x=x+b
+x-=b resta
+x/=b división
+x*=b multiplicación
+x%=b resto de una división ejemplo de 10 % 6 nos daría 4
+x**=b para exponente ejemplo 2**=10 nos da 100 => 2^10
+--------------------------------------------------------------------------------*/
+/*-----------------------------OPERADOES DE ASINGANCIÓN--------------------------
++ SUMA
+- RESTA
+* MULTIPLICACIÓN
+/ DIVISIÓN
+** EXPONENTE
+++ INCREMENTO
+-- DECREMENTO
+% PORCENTAJE
+-------------------------------------------------------------------------------*/
+/*----------METODOS PARA CONCATENAR---------------------------------------------
+- .concat sirve para concatenar ejemplo n1.concat(n2);
+- el concat solo sirve con strings ya que es un metodo de cadena
+- la forma de concatenar numeros es => "" + 13 + 14 = 1314
+----------------------------------------------------------------
+Concatenar por medio de backtiks y "${}"", los backtiks se hacen 
+con alt + 96
+*/
+/*----------OPERADORES DE COMPARACIÓN-------------------------------------------
+a == b compara si a y b son iguales si son devuelve True y sino False
+este no distingue si uno es string y el otro no siempre compara.
+a != b compara si a y b son distintos si son devuelve True y sino False.
+a === b compara si a y b son iguales si son devuelve True y sino False
+este compara que sean del mismo tipo de dato.
+a !== b compara si a y b sean distintos en tipo de dato.
+a < b compara si a es menor que b
+a > b compara si a es mayor que b
+a <= b compara si a es menor o igual que b
+a >= b compara si a es mayor o igual que b
+-------------------------------------------------------------------------------*/
+/*-----------------------OPERADORES LÓGICOS-------------------------------------
+&&--> AND --> Y
+||--> OR ---> O
+!---> NOT --> NO
+-------------------------------------------------------------------------------*/
+// Ctrl + } --> comenta las lineas de código selecionadas
+/*-----------------------Condicionales------------------------------------------
+if el bloque solo se ejecuta si se cumole la condición
+else se ejecuta en caso de no cumplirse la condición del if
+ifElse se ejecuta en caso de no cumplirse la condición del if o elseIF anterior
+while
+for
+------------------------------------------------------------------------------*/
